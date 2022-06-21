88c8749965efda518975353598b5e16484a27ed8
Igor Popov <popov_i_v@mail.ru>
2022-06-21 12:46:08 +0300

    add Topic2

---
+++ DZ/DZ2/dz2-11/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int randNum = new Random().Next(100, 1000);
+int num1 = (randNum / 100) * 10;
+int num2 = randNum % 10;
+int num3 = num1 + num2;
+Console.WriteLine($"{randNum} -> {num3}");
---
+++ S/S2/s2-9/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+9.Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. 
+78 -> 8 
+12-> 2 
+85 -> 8
+*/
+
+int randNum = new Random().Next(10,100);
+int num1 = randNum / 10;
+int num2 = randNum % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine($"{randNum} -> {num1}");
+}
+else
+{
+    Console.WriteLine($"{randNum} -> {num2}");
+}
+
